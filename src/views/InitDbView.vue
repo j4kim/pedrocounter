4a06654911705c7e2dadf99886c4c582b58d9b2d
Joaquim Perez <jivkim@gmail.com>
2026-09-24 16:26:48 +0200

    fix(InitDbView): declare newDbId ref

--- src/views/InitDbView.vue
+++ src/views/InitDbView.vue
@@ -1,30 +1,32 @@
 <script setup>
+import { ref } from "vue";
 import { dbId } from "../db";
 
+const newDbId = ref(null);
+
 function connect() {
-  dbId.value = newDbId;
+  dbId.value = newDbId.value;
 }
 </script>
 
 <template>
-  <main>
-    <div class="hero min-h-screen">
-      <div class="hero-content text-center">
-        <div class="max-w-md">
-          <h1 class="text-5xl font-bold">pedrocounter</h1>
-          <p class="py-6">
-            Premièrement, entrez le nom d'une base de données.<br />Si elle
-            n'exite pas encore, une nouvelle base de données sera initialisée.
-          </p>
-          <form class="join" @submit.prevent="connect">
-            <input
-              class="input join-item"
-              placeholder="Base de données"
-              required
-            />
-            <button class="btn join-item">Connecter</button>
-          </form>
-        </div>
+  <main class="hero min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">pedrocounter</h1>
+        <p class="py-6">
+          Premièrement, entrez le nom d'une base de données.<br />Si elle
+          n'exite pas encore, une nouvelle base de données sera initialisée.
+        </p>
+        <form class="join" @submit.prevent="connect">
+          <input
+            class="input join-item"
+            placeholder="Base de données"
+            required
+            v-model="newDbId"
+          />
+          <button class="btn join-item">Connecter</button>
+        </form>
       </div>
     </div>
   </main>
