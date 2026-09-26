7be46ea2f347dda50ca99a07e554ee4dd2ec897d
Joaquim Perez <jivkim@gmail.com>
2026-09-26 16:16:44 +0200

    layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,7 +16,7 @@ import { Cog6ToothIcon } from "@heroicons/vue/24/solid";
       </RouterLink>
     </Header>
 
-    <main class="flex flex-col gap-4 p-4">
+    <main class="mx-auto flex max-w-3xl flex-col gap-4 p-4">
       <div class="card bg-base-200 p-5 shadow" v-for="[id, item] in itemsArray">
         <div>{{ item.created_at }}</div>
         <div>{{ item.user }}</div>
